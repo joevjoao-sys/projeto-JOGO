7273341562603cda50f640c70cdfb47b665e8998
joevjoao-sys <joevjoao@gmail.com>
2025-11-07 14:43:36 -0300

    +coin

---
+++ Assets/codigos/moedacod.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ColetarMoeda2D : MonoBehaviour
+{
+   
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            
+            
+            Debug.Log("Moeda 2D coletada!");
+
+            
+            Destroy(gameObject);
+        }
+    }
+}
